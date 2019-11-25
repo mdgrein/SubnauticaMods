fc44e26fc7439d82d885f99e82b8728906d155ff
Andreas Grehn <github@accounts.mdnet.se>
2019-11-25 01:39:49 +0100

    First!

---
+++ subnautica quartz processor/QPatch.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+
+using Harmony;
+
+using MDMods.QuartzProcessor.Craftables;
+
+namespace MDMods.QuartzProcessor
+{
+    public static class QPatch
+    {
+        public static void Patch()
+        {
+            var harmony = HarmonyInstance.Create("com.quartzprocessor");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ subnautica quartz processor/QuartzProcessor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+using UnityEngine;
+
+using SMLHelper.V2.Assets;
+using SMLHelper.V2.Crafting;
+
+namespace MDMods.QuartzProcessor.Craftables
+{
+    public class QuartzProcessor : Craftable
+    {
+        public QuartzProcessor() : base("", "", "") { }
+
+        public override string AssetsFolder => $"QuartzProcessor/Assets";
+
+        protected override TechData GetBlueprintRecipe()
+        {
+            // Create and associate recipe to the new TechType
+            var customFabRecipe = new TechData()
+            {
+                craftAmount = 1,
+                Ingredients = new List<Ingredient>()
+                {
+                    new Ingredient(TechType.Quartz, 10),
+                },
+                LinkedItems = new List<TechType>()
+                {
+                    TechType.ComputerChip
+                }
+            };
+
+            return customFabRecipe;
+        }
+        public override GameObject GetGameObject()
+        {
+            GameObject prefab = GameObject.Instantiate(CraftData.GetPrefabForTechType(TechType.ComputerChip));
+
+            return prefab;
+        }
+
+        public override TechGroup GroupForPDA { get; } = TechGroup.Resources;
+
+        public override TechCategory CategoryForPDA { get; } = TechCategory.Electronics;
+
+        public override CraftTree.Type FabricatorType { get; } = CraftTree.Type.Fabricator;
+    }
+}
